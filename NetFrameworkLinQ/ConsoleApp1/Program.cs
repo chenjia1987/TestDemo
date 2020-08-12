70e66665d537c83ab6d811c32120c786e0042e22
chenjia1987 <44917852+chenjia1987@users.noreply.github.com>
2020-08-12 10:16:27 +0800

    原始代码

---
+++ NetFrameworkLinQ/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, Dictionary<int, string>> dic = new Dictionary<int, Dictionary<int, string>>()
+            {
+                { 372, new Dictionary<int, string>(){
+                    { 1, "689" },
+                    { 101, "691"}
+                } },
+                { 419, new Dictionary<int, string>(){
+                    { 1, "690" },
+                    { 101, "692"}
+                } }
+            };
+
+
+            //Dictionary<int, string> dic = new Dictionary<int, string>() { 
+            //    { 1, "Test1"},
+            //    { 2, "Test2"},
+            //    { 3, "Test3"},
+            //};
+
+            Console.WriteLine(((dic.FirstOrDefault(x => x.Key == 1)).Value)?.FirstOrDefault(x => x.Key == 1).Value);
+        }
+    }
+}
---
+++ NetFrameworkOO/NetFrameworkOO/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFrameworkOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            C c = new C() { ID = 1, Name = "A" };
+            B b = c;
+
+
+            Console.WriteLine(b.Name);
+        }
+    }
+
+    public class A
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class B : A
+    { 
+    
+    }
+
+    public class C : B { }
+}
