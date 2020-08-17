a4c88bb2c48a78eac2ab87db62b54ec7951b15ac
chenjia1987 <44917852+chenjia1987@users.noreply.github.com>
2020-08-17 18:14:02 +0800

    委托示例

---
+++ NetFrameworkDelegate/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DelegateDemo delegateDemo = new DelegateDemo();
+
+            MethodWithCallback(1, 2, DelegateDemo.ConsoleWriteLine);
+        }
+
+        public static void MethodWithCallback(int param1, int param2, Del callback)
+        {
+            callback("The number is: " + (param1 + param2).ToString());
+        }
+    }
+}
